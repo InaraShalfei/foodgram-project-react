46b010a6503bfbe0ab47e15526f4e289bb24ab99
Инара <inarashugaikina@gmail.com>
2021-07-30 13:15:36 +0600

    Create start.sh and change logic in backend/Dockerfile

---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python manage.py makemigrations users
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
