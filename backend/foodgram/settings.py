b6c8d4124705c57f7906253012f0265491f768f6
Инара <inarashugaikina@gmail.com>
2021-07-31 20:03:07 +0600

    Add default pagination into settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -143,9 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+        'rest_framework.authentication.TokenAuthentication'),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
 }
+
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
