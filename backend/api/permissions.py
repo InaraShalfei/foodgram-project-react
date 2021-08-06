b6aaef2c32104f230a6cf688499e9488a1b9bc74
InaraShalfei <inarashugaikina@gmail.com>
2021-08-06 13:34:15 +0500

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,4 +9,4 @@ class OwnerOrReadOnly(permissions.BasePermission):
             )
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return obj.author == request.user
