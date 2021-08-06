6922eee1e3f13d144a6971a41afbc1f37b98b507
InaraShalfei <inarashugaikina@gmail.com>
2021-08-06 13:02:55 +0500

    Update admin panel for all models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
+from users.models import User
 from .models import Ingredient, Tag, Recipe
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('username', 'email')
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    list_filter = ('author', 'name', 'tags')
+    empty_value_display = '-пусто-'
+
 
 admin.site.register(Tag)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
+admin.site.register(User)
