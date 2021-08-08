f626870dd1a7e902168f262be1e4594992d57ebc
InaraShalfei <inarashugaikina@gmail.com>
2021-08-08 16:08:19 +0500

    Add ordering into Recipe model

--- backend/api/models.py
+++ backend/api/models.py
@@ -57,8 +57,10 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredients')
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredients')
     amount = models.PositiveIntegerField(verbose_name='amount of ingredient')
+    creation_date = models.DateTimeField("Date of creation", auto_now_add=True, db_index=True)
 
     class Meta:
+        ordering = ['-creation_date']
         verbose_name = 'Ingredient in recipe'
         verbose_name_plural = 'Ingredients in recipe'
         constraints = [
