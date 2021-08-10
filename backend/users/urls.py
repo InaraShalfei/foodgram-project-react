96a9eca0453892cb0ee484a136c2b5147db763b1
InaraShalfei <inarashugaikina@gmail.com>
2021-08-10 16:19:21 +0500

    Create urls for subscription in users app

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from django.urls import include, path
 
+from users.views import UserViewSet
+
+subscribe = UserViewSet.as_view({
+    'get': 'subscribe',
+    'delete': 'subscribe'
+})
+
 urlpatterns = [
     path('', include('djoser.urls')),
     url(r'^auth/', include('djoser.urls.jwt')),
     url(r'^auth/', include('djoser.urls.authtoken')),
+    path('users/<int:pk>/subscribe/', subscribe, name='subscribe'),
 ]
