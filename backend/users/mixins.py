dbb85c49611013d42d424d817b26aaafc647cbd4
InaraShalfei <inarashugaikina@gmail.com>
2021-08-10 20:23:49 +0500

    Create mixin class for is_subscribed

---
+++ backend/users/mixins.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
+
+from api.models import UserFollow
+
+
+class IsSubscribedMixin(Serializer):
+    is_subscribed = serializers.SerializerMethodField('get_is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        return UserFollow.objects.filter(followed=obj, follower=self.context.get('request').user).exists()
