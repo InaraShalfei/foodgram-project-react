82c0c9465193b0ef1fdaac2685a7a9917bc8ef98
InaraShalfei <inarashugaikina@gmail.com>
2021-08-10 20:22:53 +0500

    Delete empty_fields from api/admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -6,19 +6,16 @@ from .models import Ingredient, Tag, Recipe
 
 class UserAdmin(admin.ModelAdmin):
     list_filter = ('username', 'email')
-    empty_value_display = '-пусто-'
 
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
-    empty_value_display = '-пусто-'
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author')
     list_filter = ('author', 'name', 'tags')
-    empty_value_display = '-пусто-'
 
 
 admin.site.register(Tag)
