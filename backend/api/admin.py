68f95db14be5f6b1577ec0027ce1fb829595d8e0
InaraShalfei <inarashugaikina@gmail.com>
2021-08-12 10:14:59 +0500

    Add many-to-many through field into django admin zone

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,24 +1,32 @@
 from django.contrib import admin
 
 from users.models import User
-from .models import Ingredient, Tag, Recipe
+from .models import Ingredient, Tag, Recipe, RecipeIngredient
 
 
 class UserAdmin(admin.ModelAdmin):
     list_filter = ('username', 'email')
 
 
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
+    inlines = (RecipeIngredientInline, )
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author')
     list_filter = ('author', 'name', 'tags')
+    inlines = (RecipeIngredientInline,)
 
 
 admin.site.register(Tag)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(User, UserAdmin)
+admin.site.register(RecipeIngredient)
