8304b2b2374b92ed89d83b025a59cd96ec3e3359
InaraShalfei <inarashugaikina@gmail.com>
2021-08-15 19:38:41 +0500

    Create fields.py for image field and tag colors serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,52 +1,11 @@
-import imghdr
-import base64
-import six
-import uuid
-import webcolors as webcolors
-
 from rest_framework import serializers
-from django.core.files.base import ContentFile
 
+from api.fields import Hex2NameColor, Base64ImageField
 from api.models import (FavoriteRecipe, Ingredient, Recipe,
                         RecipeIngredient,  ShoppingCart, Tag)
 from users.mixins import IsSubscribedMixin
 from users.models import User
 from users.serializers import CustomUserSerializer
-
-
-class Hex2NameColor(serializers.Field):
-    def to_representation(self, value):
-        return value
-
-    def to_internal_value(self, data):
-        try:
-            data = webcolors.hex_to_name(data)
-        except ValueError:
-            raise serializers.ValidationError('Для этого цвета нет имени')
-        return data
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, six.string_types):
-            if 'data:' in data and ';base64,' in data:
-                header, data = data.split(';base64,')
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-            file_name = str(uuid.uuid4())[:12]
-            file_extension = self.get_file_extension(file_name, decoded_file)
-            complete_file_name = '%s.%s' % (file_name, file_extension,)
-            data = ContentFile(decoded_file, name=complete_file_name)
-
-        return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-        extension = imghdr.what(file_name, decoded_file)
-        extension = 'jpg' if extension == 'jpeg' else extension
-
-        return extension
 
 
 class TagSerializer(serializers.ModelSerializer):
