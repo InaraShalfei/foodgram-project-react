badb09dd386ab8a850a8cd230fd839a6d2120d00
InaraShalfei <inarashugaikina@gmail.com>
2021-08-15 19:11:19 +0500

    Add verbose names into User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 
 class User(DjangoUser):
-    email = models.EmailField(blank=False, unique=True)
+    email = models.EmailField(blank=False, unique=True, verbose_name='user email')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
