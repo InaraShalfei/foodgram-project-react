5c79063f8578779690770053ddc984a655113d11
InaraShalfei <inarashugaikina@gmail.com>
2021-08-17 15:04:18 +0500

    Change logic in permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,4 +9,7 @@ class OwnerOrReadOnly(permissions.BasePermission):
             )
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+        )
