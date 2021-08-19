8282c042f592113603417e233f8b7763074599cf
InaraShalfei <inarashugaikina@gmail.com>
2021-08-19 11:43:11 +0500

    Add new allowed host into settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = (
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', '84.201.128.102']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web', '178.154.231.36']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
