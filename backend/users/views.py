319edf26e20821e8d3763b11a7af37ddf5356404
InaraShalfei <inarashugaikina@gmail.com>
2021-08-20 23:26:13 +0500

    Delete unused import in users app

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,6 @@ import djoser.views
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status
 from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.serializers import ListSerializer
 
